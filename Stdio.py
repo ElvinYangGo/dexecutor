951d48e8ddffdc8c1ee4c05c41b71265e5526294
elvin yang go <elvin.yang.go@gmail.com>
2011-04-19 20:33:03 +0800

    add read function for Stdio

---
+++ Stdio.py
@@ -0,0 +1,43 @@
+import sys
+import socket
+import threading
+import os
+
+def getInputAndSendToSock(sock):
+	while True:
+		lineData = sys.stdin.readline()
+		sock.sendall(lineData.encode('ascii'))
+		'''
+		while 0 < len(lineData):
+			sentSize = sock.send(lineData.encode('ascii'))
+			lineData = lineData[sentSize:]
+		'''
+
+
+class Stdio:
+	def __init__(self):
+		self.initServerSock()
+		self.initInputSockAndThread()
+
+	def initServerSock(self):
+		self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.server.bind(('127.0.0.1', 0))
+		self.server.listen(5)
+		self.port = self.server.getsockname()[1]
+
+	def initInputSockAndThread(self):
+		self.initInputSock()
+		self.initInputThread()
+
+	def initInputSock(self):
+		self.clientSockForInput = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.clientSockForInput.connect(('127.0.0.1', self.port))
+		self.serverSockForInput, addr = self.server.accept()
+	
+	def initInputThread(self):
+		inputThread = threading.Thread(target = getInputAndSendToSock, args=(self.clientSockForInput,))
+		inputThread.start()
+
+	def read(self):
+		data = self.serverSockForInput.recv(1024)
+		return data.decode('ascii').replace(os.linesep, '')
--- tests/test_Stdio.py
+++ tests/test_Stdio.py
@@ -1,3 +1,33 @@
-import unittest
+import os
+import sys
 
+parent_path = os.path.join(os.path.dirname(sys.argv[0]), os.pardir)
+absolute_parent_path = os.path.abspath(parent_path)
+sys.path.append(absolute_parent_path)
+
+import unittest
+from mock import Mock
+import Stdio
+
+#print(dir(sys.stdin))
+class StdioTest(unittest.TestCase):
+	def setUp(self):
+		"""
+		self.mockStdin = Mock()
+		self.mockStdin.readline.return_value = 'aaaaaaa' 
+		self.originalStdin = sys.stdin
+		sys.stdin = self.mockStdin
+		"""
+
+	def tearDown(self):
+		pass
+		#sys.stdin = self.originalStdin
+
+	def testreadline(self):
+		stdio = Stdio.Stdio()
+		s = stdio.read()
+		self.assertEqual('aaa', s)
+
+if __name__ == '__main__':
+	unittest.main()
 
