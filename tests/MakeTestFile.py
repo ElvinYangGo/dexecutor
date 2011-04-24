aaa2dda4f0521bd6fce145e98201e0ce54161c62
elvin yang go <elvin.yang.go@gmail.com>
2011-04-24 17:47:52 +0800

    clear MakeTestFile

--- tests/MakeTestFile.py
+++ tests/MakeTestFile.py
@@ -3,20 +3,6 @@ import sys
 if '__main__' == __name__:
 	fileName = sys.argv[1]
 	testFile = open(fileName+'.py', 'w')
-	"""
-import Support
-import unittest
-from BufferHeadDecoder import BufferHeadDecoder
-class BufferHeadDecoder(unittest.TestCase):
-	def testDecode(self):
-		pass
-
-def getTests():
-	return unittest.makeSuite(BufferHeadDecoderTest)
-
-if '__main__' == __name__:
-	unittest.main()
-	"""
 
 	testFile.write('import Support\n')
 	testFile.write('import unittest\n')
@@ -32,4 +18,3 @@ if '__main__' == __name__:
 	testFile.write('	unittest.main()')
 
 	testFile.close()
-
